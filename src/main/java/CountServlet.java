647f65a483e8cb8537c59f3326da8f154495e2c1
Shelby Polasek <shelbypolasek@gmail.com>
2020-07-14 12:03:46 -0500

    finished bonus

---
+++ src/main/java/CountServlet.java
@@ -0,0 +1,30 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CountServlet", urlPatterns = "/count")
+public class CountServlet extends HttpServlet {
+
+    int counter = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String reset = request.getParameter("reset");
+
+        if (reset != null) {
+            counter = 0;
+        }
+        counter++;
+
+        out.println("<p> count: " + counter + "</p>");
+    }
+
+}
+
+
